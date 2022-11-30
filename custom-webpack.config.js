d95df9449fa749f76cd63eb50162deb518921c00
zinderud <mokosam@gmail.com>
2022-11-30 15:40:21 +0300

    fix dependency

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+  module.exports = {
   resolve: {
     fallback: {
-      crypto: require.resolve('crypto-browserify')
+      crypto: require.resolve('crypto-browserify'),
+      stream:require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify'),
+      assert: require.resolve('assert'),
     },
   },
-};
+}; 
