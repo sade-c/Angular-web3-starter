6fbce7898edd972744ef7bdb6f1f432ac5c32c57
zinderud <mokosam@gmail.com>
2022-11-30 21:07:15 +0300

    depends hell

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,12 +1,7 @@
-  module.exports = {
+module.exports = {
   resolve: {
     fallback: {
-      crypto: require.resolve('crypto-browserify'),
-      stream:require.resolve('stream-browserify'),
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      os: require.resolve('os-browserify'),
-      assert: require.resolve('assert'),
+      crypto: require.resolve('crypto-browserify')
     },
   },
-}; 
+};
